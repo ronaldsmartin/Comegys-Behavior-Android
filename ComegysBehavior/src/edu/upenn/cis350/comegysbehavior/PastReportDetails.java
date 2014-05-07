9b2f8e09211a2528b1c40e08c519fe5262e01306
BrianSladek <brian.sladek@gmail.com>
2014-05-07 00:42:14 -0400

    added email functionality to past reports

--- ComegysBehavior/src/edu/upenn/cis350/comegysbehavior/PastReportDetails.java
+++ ComegysBehavior/src/edu/upenn/cis350/comegysbehavior/PastReportDetails.java
@@ -1,23 +1,33 @@
 package edu.upenn.cis350.comegysbehavior;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class PastReportDetails extends Activity {
+	
+	protected Report pastReport;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.past_reports_details);
 
 		// Retrieve brother data from intent.
-		final Report pastReport = (Report) getIntent().getParcelableExtra(getString(R.string.past_report_data));
+		pastReport = (Report) getIntent().getParcelableExtra(getString(R.string.past_report_data));
 
 		// Set all labels.
 		//Set Name
@@ -75,7 +85,63 @@ public class PastReportDetails extends Activity {
 		String strategyCom =  pastReport.strategyComment;
 		strategiesComLabel.setText(strategyCom);
 
-		
+		final Button button = (Button) findViewById(R.id.email_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+           
+           	 getUserEmailPrompt();
+            }
+        });
 	
+	}
+	
+protected void sendEmail(String emailAddress) {
+		      Log.i("Send email", "");
+
+		      String[] TO = {emailAddress};
+		      String[] CC = {};
+		      Intent emailIntent = new Intent(Intent.ACTION_SEND);
+		      emailIntent.setData(Uri.parse("mailto:"));
+		      emailIntent.setType("text/plain");
+
+
+		      emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+		      emailIntent.putExtra(Intent.EXTRA_CC, CC);
+		      emailIntent.putExtra(Intent.EXTRA_SUBJECT, "New Student Behavior Report");
+		      emailIntent.putExtra(Intent.EXTRA_TEXT, pastReport.createEmailReportString());
+
+		      try {
+		         startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+		         
+		         Log.i("Finished sending email...", "");
+		      } catch (android.content.ActivityNotFoundException ex) {
+		         Toast.makeText(this, 
+		         "There is no email client installed.", Toast.LENGTH_SHORT).show();}
+		      }
+	
+	private void getUserEmailPrompt() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		
+		alert.setTitle("Send to Email");
+		alert.setMessage("If you would like to receive a copy of this report by email, please enter your email address below.");
+
+		// Set an EditText view to get user input 
+		final EditText input = new EditText(this);
+		alert.setView(input);
+
+		alert.setPositiveButton("Send email", new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int whichButton) {
+		  String emailAddress = input.getText().toString();
+		  sendEmail(emailAddress);
+		  }
+		});
+
+		alert.setNegativeButton("Don't email", new DialogInterface.OnClickListener() {
+		  public void onClick(DialogInterface dialog, int whichButton) {
+		    // Canceled.
+		  }
+		});
+
+		alert.show();
 	}
 }
